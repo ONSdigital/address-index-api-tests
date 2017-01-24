b94af34d74638cf988fdca5c2f7c790e37dda441
Al West <al.west@valtech.co.uk>
2017-01-24 11:44:32 +0000

    Update AddressIndexSearch.java

--- src/test/java/uk/gov/ons/addressindextest/Pages/AddressIndexSearch.java
+++ src/test/java/uk/gov/ons/addressindextest/Pages/AddressIndexSearch.java
@@ -13,7 +13,7 @@ public class AddressIndexSearch extends PageBase {
     }
     
     public void visitPage() {
-        driver.get("https://addressindexui.cfapps.io/addresses");
+        driver.get("https://addressindex-uicfapps.io/addresses");
     }
 
     public String title = "Address index demo - Find an address";
