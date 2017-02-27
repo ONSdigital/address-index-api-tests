4a92c5a9fa52d5f72033beca4086d66a15afc655
Al West <al.west@valtech.co.uk>
2017-02-27 10:16:49 +0000

    Create AddressIndexClerical.java

---
+++ src/test/java/uk/gov/ons/addressindextest/Pages/AddressIndexClerical.java
@@ -0,0 +1,42 @@
+package uk.gov.ons.addressindextest.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AddressIndexSearch extends PageBase {
+
+    public AddressIndexSearch(WebDriver driver) {
+        this.driver = driver;
+    }
+    
+    public void visitPage() {
+        driver.get("https://addressindex-ui.cfapps.io/clerical/search");
+    }
+
+    public String title = "Address index demo - Find an address";
+
+    public static String getTitle(WebDriver driver) {
+        return driver.getTitle();
+    }
+
+    @FindBy(id = "submit")
+    private WebElement submitButton;
+
+    @FindBy(id = "address")
+    private WebElement searchTextAreaInput;
+
+    @FindBy(className = "green")
+    private WebElement resultsSpan;
+
+    public void submitSearchText(String text) {
+        setTextAreaInputValue(this.searchTextAreaInput, text);
+        clickButton(this.submitButton);
+    }
+
+    public String getResultsText() {
+        return this.resultsSpan.getText();
+    }
+}
