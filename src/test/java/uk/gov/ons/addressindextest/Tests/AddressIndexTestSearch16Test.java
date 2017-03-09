7f092d6aa44cce4d78f6b416f3cc6b359c537584
Al West <al.west@valtech.co.uk>
2017-03-09 10:49:46 +0000

    Update and rename AddressIndexTestSearchTest16.java to AddressIndexTestSearch16Test.java

--- src/test/java/uk/gov/ons/addressindextest/Tests/AddressIndexTestSearch16Test.java
+++ src/test/java/uk/gov/ons/addressindextest/Tests/AddressIndexTestSearch16Test.java
@@ -20,14 +20,14 @@ import java.util.UUID;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
-public class AddressIndexTestSearchTest16 extends AddressIndexTestBase {
+public class AddressIndexTestSearch16Test extends AddressIndexTestBase {
 
     /**
      * Runs a simple test verifying the search input is functional.
      * @throws InvalidElementStateException
      */
     @org.testng.annotations.Test(dataProvider = "hardCodedBrowsers")
-    public void verifySearchTest16(String browser, String version, String os, Method method)
+    public void verifySearch16Test(String browser, String version, String os, Method method)
     throws MalformedURLException, InvalidElementStateException, UnexpectedException {
 
         String uprn = "10013038967";
