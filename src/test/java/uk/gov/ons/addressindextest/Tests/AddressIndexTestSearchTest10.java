80ca62b0c7c83f43b5530df5ae0da108adadb757
Al West <al.west@valtech.co.uk>
2017-03-09 09:23:03 +0000

    Update AddressIndexTestSearchTest10.java

--- src/test/java/uk/gov/ons/addressindextest/Tests/AddressIndexTestSearchTest10.java
+++ src/test/java/uk/gov/ons/addressindextest/Tests/AddressIndexTestSearchTest10.java
@@ -20,18 +20,18 @@ import java.util.UUID;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
-public class AddressIndexTestSearchTest extends AddressIndexTestBase {
+public class AddressIndexTestSearchTest10 extends AddressIndexTestBase {
 
     /**
      * Runs a simple test verifying the search input is functional.
      * @throws InvalidElementStateException
      */
     @org.testng.annotations.Test(dataProvider = "hardCodedBrowsers")
-    public void verifySearchTest(String browser, String version, String os, Method method)
+    public void verifySearchTest10(String browser, String version, String os, Method method)
     throws MalformedURLException, InvalidElementStateException, UnexpectedException {
 
-        String uprn = "100040239491";
-        String searchInputText = "Flat F 27 Well Street";
+        String uprn = "10013050229";
+        String searchInputText = "31, Constantine House, New North Road, Exeter, Devon EX4 4JH";
 
         this.createDriver(browser, version, os, method.getName());
         WebDriver driver = this.getWebDriver();
