ff03c8211f0b23b2fad019f5b765e3aaef017cb7
Al West <al.west@valtech.co.uk>
2017-03-13 09:31:15 +0000

    Added script to generate new test cases

---
+++ src/test/java/uk/gov/ons/addressindextest/Tests/MakeTestFromTemplate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo "Please pass parameters (3) for test cases number, UPRN and address"
+    exit 1
+fi
+
+filename="AddressIndexTestSearch${1}Test.java"
+echo $filename
+if [ -f $filename ]; then
+    echo "Filename already exists"
+    exit 2
+fi
+
+sed "s/<NO>/${1}/g" AddressIndexTestSearchTestTemplate.template > $filename
+sed -i "s/<UPRN>/${2}/g" $filename
+sed -i "s/<ADDRESS>/${3}/g" $filename
+
